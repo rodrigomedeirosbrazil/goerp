5a99965a75f00ad045dd6b623f7989a913cedecc
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-08-20 19:35:03 -0300

    chore: add simple auth jwt

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"goerp/internal/auth"
+
 	"github.com/gofiber/fiber/v2"
+
+	jwtware "github.com/gofiber/contrib/jwt"
 )
 
 func main() {
@@ -10,6 +14,13 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Welcome to GOERP!")
 	})
+
+	app.Post("/login", auth.Login)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+	}))
 
 	app.Listen(":3000")
 }
---
+++ internal/auth/auth.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func Login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Throws Unauthorized error
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name":  "John Doe",
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
